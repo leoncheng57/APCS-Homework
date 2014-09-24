15797da67df94126ea587f78100fadcc1d81aac6
leoncheng57 <57leonardo@gmail.com>
2014-09-24 08:41:26 -0400

    added wed stuff

--- 05-Coding_Bat/StringStuff.java
+++ 05-Coding_Bat/StringStuff.java
@@ -26,5 +26,41 @@ public class StringStuff {
 	if (n > 21) return 2*absDiff;
 	else return absDiff;
   
+
+//assignments on Wed
+/* working */
+    public boolean nearHundred(int n) {
+	if ((n>=90) && (n<=110)) return true;
+	if ((n>=190) && (n<=210)) return true;
+	else return false;
+    }
+/* working */
+    public boolean mixStart(String str) {
+	if (str.length() < 3) return false;
+	if (str.substring(1, 3).equals("ix")) 
+	    {return true;
+	    }
+	else
+	    {return false;
+	    }
+    }
+/* working */
+    public int teaParty(int tea, int candy) {
+ 
+	if ((tea < 5) || (candy < 5)) return 0;
+	if (((tea / 2) >= candy) || ((candy / 2) >= tea)) return 2;
+	else return 1;
+    }
+/* working */
+    public boolean lastDigit(int a, int b, int c) {
+	a = a % 10;
+	b = b % 10;
+	c = c % 10;
+	if ( (a == b) || (a == c) || (b == c) ) return true;
+	else return false;
+    }
+
+
+
     }
 }
