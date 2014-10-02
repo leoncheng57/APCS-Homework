91128a1f69528387c2078ba2eaf676fe10ea1ec4
leoncheng57 <57leonardo@gmail.com>
2014-10-02 19:30:45 -0400

    rearranged files to correct order

--- 07-Shapes/Driver.java
+++ 07-Shapes/Driver.java
@@ -1,4 +1,23 @@
+public class Driver {
+    public static void main(String[] args){
+	Shapes s = new Shapes();
+	System.out.println(s.tri1(4));
+	System.out.println(s.tri2(4));
+	System.out.println(s.tri3(4));
+	System.out.println(s.tri4(4));
 
+	System.out.println(s.diamond(1));
+	System.out.println(s.diamond(2));
+	System.out.println(s.diamond(3));
+	System.out.println(s.diamond(4));
+	System.out.println(s.diamond(5));
+	System.out.println(s.diamond(6));
+	
+    }
+}
+
+//Shapes Assignment Part 1
+/*
 public class Driver {
     public static void  main(String[] args){
 	Shapes s =  new Shapes();
@@ -18,3 +37,4 @@ public class Driver {
         
     }
 }
+*/
