d7d1f1268a343dacdb06bf1fb7e930797d942246
leoncheng57 <57leonardo@gmail.com>
2014-10-02 08:42:30 -0400

    started

---
+++ 07b/Shapes.java
@@ -0,0 +1,27 @@
+public class Shapes {
+   
+    public String tri3 (int h) {
+	int i = 0;
+	int x = 0;
+	int rowLength = h;
+	String  s = "";
+	while (i<h-1){
+	    x = 0;
+	    while (x<rowlength-1){
+		s = s +".";
+		x++;
+	    }
+	}
+	/*
+	
+	while (i<h){
+	    while (x<rowLength/2){
+		s = s + " ";
+		x = x+1
+	    }
+	    
+	    }
+
+	*/
+    }
+}
