0a442b87c6fff6d90e836f342a8044f14d5a92cb
leoncheng57 <57leonardo@gmail.com>
2014-11-08 20:22:34 -0500

    finished hw

--- 15-Sarray/Sarray.java
+++ 15-Sarray/Sarray.java
@@ -1,21 +1,55 @@
-/*<----------------My Notes------------------->*/
+/*<----------------MY NOTES------------------->*/
 /*
   lists are not the same as arrays
   lists are like the things we used in Python (easier to use than Arrays)
-  lists are made from arrays
+  lists are made 
 */
-
-
-public class Sarray{
+public class Sarray{    
+    /*<----------------MAIN------------------->*/
+    public static void main(String[] args){
+     	Sarray s = new Sarray();
+	System.out.println(s);
+	s.set(8,11); //set 
+	System.out.println(s);
+	System.out.println(s.get(8)); //get
+	System.out.println(s);
+	System.out.println(s.add(22));//add(version1)
+	System.out.println(s);
+	System.out.println(s.add(22));//add(version1)
+	System.out.println(s);
+	s.add(3,33);//add(version2)
+	System.out.println(s);    
+	s.add(15,33);//add(version2)
+	System.out.println(s);    
+	s.remove(4); //remove
+	s.remove(5); //remove
+	System.out.println(s);
+	System.out.println("the size is: " + s.size()); //size	
+    }
+    
+    /*<----------------INSTANCE VARIABLES------------------->*/
     private int[] data; // should be object[]
-    private int last; //last index of the list TODO:??Where do we use this??
+    private int last; //last index of the list
 
+    /*<----------------METHODS AND CONSTRUCTOR------------------->*/
+    //This was a method that I decided to include
+    //prints the array in a readable fashion
+    //finds the number for last, changes last, prints last
     public String toString(){
 	String ret = ""; 
 	for (int a=0;a<data.length;a++){
     	    ret += +data[a]+", ";
 	}
-	ret+="\n"+"last: "+last;
+	ret+="\n";
+	int count = -1;
+	for (int a=0;a<data.length;a++){
+	    if (data[a]!=0){
+		count = a;
+	    }
+	}
+	last = count;	
+	ret+="last: "+last;
+	ret+="\n";
 	return ret;
     }
 
@@ -23,102 +57,90 @@ public class Sarray{
     public Sarray() {
 	// start array at size 10, don't use 0
 	data = new int[10];
-	last = data.length;
     }
 
     //adds an item to the end of the list, grow if needed, returns true
     public boolean add (int i) {
-	boolean done = false;
-	for (int a=0;done==false;a++){
-	    if (data[a]==0) {
-		data[a]=i;
-		done = true;
-	    }
-	    //if all the elements in data are full, make a new array with one more element at the end
-	    if (a==data.length-1){
-		int[] newArray = int[data.length+1];
-		for (int b=0;b<newArray.length;b++){
-		    newArray[b]=data[b];
-		}
-		newArray[newArray.length-1]=i;
-		data = newArray;
-	    }
+	//Note: The toString fxn usually needs to be run before this function, b/c this relies in the var last, which is updated in toString
+	if (last == data.length-1){
+	    //gotta create a new array with i as the last added element
+	    int[] newArray = new int[data.length+1];
+	    for (int a=0;a<data.length;a++){newArray[a]=data[a];}
+	    newArray[newArray.length-1] = i;
+	    data=newArray;
 	}
-	return done;	   
+	else {data[last+1]=i;}
+	return true;
     }
 
     //adds item i at index, shifting everything down as needed
     //also grows as needed
     //!!TODO:need to implement growing!!
     public void add (int index, int i) {
-	// inserts at location index - shift everything else down
-	int[] newarray=new int[data.length+1];
-	for (int a=0;a<data.length;a++) {
-	    if (a<index) {
-		newarray[a]=data[a];
-	    }
-	    if (a==index) {
-		newarray[a]=i;
-	    }
-	    if (a>index) {
-		newarray[a]=data[a-1];
+    	// inserts at location index - shift everything else down
+    	int[] newArray;
+	if (index<data.length){
+	    newArray=new int[data.length+1];
+	    for (int a=0;a<newArray.length;a++) {
+		if (a<index) {
+		    newArray[a]=data[a];
+		}
+		if (a==index) {
+		    newArray[a]=i;
+		}
+		if (a>index) {
+		    newArray[a]=data[a-1];
+		}
 	    }
 	}
-	data = newarray;
+	else {
+	    newArray = new int[index+1];
+	    for (int a=0;a<data.length;a++){
+		newArray[a]=data[a];
+	    }
+	    newArray[index]=i;
+	}
+    	data = newArray;
     }
 
     //return the number of items in the list (not the array size)
     public int size(){
-        for (int a=0;a<data.length;a++) {
-	    if (data[a]==0) {
-		return a;
+	int count = 0;
+	for (int a=0;a<data.length;a++){
+	    if (data[a]!=0){
+		count++;
 	    }
 	}
-	return -1; //Do I need this????
+	return count;
     }
 
     //returns the item at location index of the list
     public int get (int index) {
-	// gets the item
 	return data[index];
     }
     
     //sets the item at locaiton index to value i
     //returns the old value
-    public void set (int index, int i) {
-	// replaces item
+    public int set (int index, int i) {
+	int ret = data[index];
 	data[index]=i;
+	return ret;
     }
         
     //removes the item at index i
-    //returns the old value
-    public void remove (int index) {
-	// must shift things over
-	for (int a=0;a<data.length-1;a++) {
-	    if (a>=index) {
-		data[a]=data[a+1];
-	    }
-	}
-	data[data.length-1]=0;
-    }
-
-
-
-    //MAIN
-    public static void main(String[] args){
-     	Sarray s = new Sarray();
-	System.out.println(s);
-	s.add(1);
-	s.add(1);
-	s.add(1);
-	s.add(1);
-	s.add(1);
-	s.add(1);
-	s.add(1);
-	s.add(1);
-	s.add(1);
-	s.add(1);
-	System.out.println(s.add(1));
-	System.out.println(s);
+    //returns the old value 
+    public int remove(int index){
+	int ret = data[index];
+	int[] newArray = new int[data.length-1];
+	// for(int a=0;a<newArray.length;a++){
+	//     if (a<index){newArray[a]=data[a];}
+	//     if (a>=index){newArray[a]=data[a+1];}
+	// }
+	int a=0;
+	for (a=a;a<index;a++){newArray[a]=data[a];}
+	for (a=a;a<newArray.length;a++){newArray[a]=data[a+1];}
+	data = newArray;
+	return ret;
     }
 }
+
