ae6bc9d1acaced9c6350e2ef9f3e4edd04ef4785
leoncheng57 <57leonardo@gmail.com>
2014-11-19 21:22:12 -0500

    finished hw

--- 18-WordSearch/WordSearch.java
+++ 18-WordSearch/WordSearch.java
@@ -30,36 +30,61 @@ public class WordSearch{
 	return s;
     }
 
-    public void addWordH(String w,int row, int col){
+    public void addWordH(String w, String direction, int row, int col){
 	int r = row, c = col;
-	boolean doesOverlap = false;
+	boolean doesNotOverlap = true;
 
 	for (int i=0;i<w.length();i++){
+	    if (r>=board.length){
+		doesNotOverlap = false;
+		break;
+	    }
+	    if (c>=board[r].length){
+		doesNotOverlap = false;
+		break;
+	    }	
 	    if (board[r][c]!='.') {
-		doesOverlap = true;
+		doesNotOverlap = false;
+		break;
 	    }
 	    c++;
 	}
 
-	System.out.println(doesOverlap);
+	System.out.println(doesNotOverlap);
 
 	r = row;
 	c = col;
-	if (!doesOverlap){
-	    for (int i=0;i<w.length();i++){
-		board[r][c] = w.charAt(i);
-		c++;
+	if (direction == "f"){ 
+	    if (doesNotOverlap){
+		for (int i=0;i<w.length();i++){
+		    board[r][c] = w.charAt(i);
+		    c++;
+		}
 	    }
 	}
-
+	if (direction == "b") {
+	    if (doesNotOverlap){
+		for (int i=w.length()-1;i>=0;i--){
+		    board[r][c] = w.charAt(i);
+		    c++;
+		}
+	    }
+	}
     }
 
     public static void main(String[] args) {
 	WordSearch w = new WordSearch();
 	System.out.println(w);
-	w.addWordH("hello",3,5);
-	w.addWordH("look",3,8);
-	w.addWordH("look",3,7);
+	w.addWordH("hello", "f", 3,5);
+	w.addWordH("look", "f",3,8);
+	w.addWordH("look", "f",11,7);
+	w.addWordH("look", "f",99,7);
+	w.addWordH("look", "f",2,99);
+	w.addWordH("look", "f",1,29);
+	w.addWordH("look", "b",2,5);
+	w.addWordH("look", "b",7,6);
+	w.addWordH("look", "b",1,1);
+	w.addWordH("look", "bleh",1,1);
 	//w.addWordH("hello",100,5);
 	//w.addWordH("hello",30,555);
 				
