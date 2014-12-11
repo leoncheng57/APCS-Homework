54e212b22337f11a0bfde37d5d3fdc6ae1f0f9ae
leoncheng57 <57leonardo@gmail.com>
2014-12-10 20:47:16 -0500

    improved hw

---
+++ 21-SortingStuyabloCharacters/ExtendBaseChar.java
@@ -0,0 +1,8 @@
+public class ExtendBaseChar extends BaseChar { //extending BaseChar works! we can still use compareTo from BaseChar
+    
+    public String toString(){
+	//System.out.println("I am an instance of ExtendBaseChar");
+	return name;
+    }
+    
+}
--- 21-SortingStuyabloCharacters/sortingTest.java
+++ 21-SortingStuyabloCharacters/sortingTest.java
@@ -2,9 +2,23 @@ import java.util.*;
 import java.io.*;
 public class sortingTest{
     public static void main(String[] args){
-	BaseChar[] a = new BaseChar[5];
+	// BaseChar[] a = new BaseChar[5];
+	// for (int i=0;i<a.length;i++){
+	//     a[i]=new BaseChar();
+	// }
+	// a[0].setName("myname");
+	// a[1].setName("bobito");
+	// a[2].setName("doyle");
+	// a[3].setName("margo");
+	// a[4].setName("parsons");
+	// System.out.println(Arrays.toString(a));
+	// Arrays.sort(a);
+	// System.out.println(Arrays.toString(a));
+	
+	//What if we extend the class???
+	ExtendBaseChar[] a = new ExtendBaseChar[5];
 	for (int i=0;i<a.length;i++){
-	    a[i]=new BaseChar();
+	    a[i]=new ExtendBaseChar();
 	}
 	a[0].setName("myname");
 	a[1].setName("bobito");
@@ -14,5 +28,7 @@ public class sortingTest{
 	System.out.println(Arrays.toString(a));
 	Arrays.sort(a);
 	System.out.println(Arrays.toString(a));
+	
+
     }
 }
