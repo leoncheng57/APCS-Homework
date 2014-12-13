767bd14dce6ddbbe2503056c95c04187f3695e01
leoncheng57 <57leonardo@gmail.com>
2014-12-13 18:05:44 -0500

    finished concepts of hw

--- 22-Searching/Search.java
+++ 22-Searching/Search.java
@@ -1,14 +1,78 @@
+import java.util.*;
+import java.io.*;
+
 public class Search{
-    public Comparable isearch(ArrayList<Comparable> a,Comparable value){
-	for (int i=0;i<a.size();i++){
-	    if (a.get(i)==value){
-		return value;
+
+    //linera search
+    public Comparable lsearch (Comparable[] a, Comparable s){
+	for (int i=0;i<a.length;i++){
+	    if (a[i].equals(s)){
+		return s;
 	    }
 	}
 	return null;
     }
 
+    //binary search
+    public Comparable bsearch(Comparable[] a, Comparable s){
+	int low = 0;
+	int high = a.length-1;
+	int mid;
+	int n;
+	while (low!=high){
+	    mid = (low+high)/2;
+	    n = s.compareTo(a[mid]);
+	    if (n==0)
+		return s;
+	    if (n>0)
+		low = mid+1;
+	    if (n<0)
+		high = mid-1;
+	}
+	if (s.compareTo(a[low])==0){
+	    return s;
+	}
+	else
+	    return null;
+    }
+    
+    //recrusive binary search
+    public Comparable rbsearch(Comparable[] a, Comparable s){
+	return rbsearch(a,s,0,a.length-1); //why does "return" have to be in the code?
+    }
+
+    public Comparable rbsearch(Comparable[] a, Comparable s,int low, int high){
+	int mid, n;
+	while (low != high){
+	    mid = (low + high)/2;
+	    n = s.compareTo(a[mid]);
+	    if (n==0)
+		return s;
+	    if (n>0)
+		return rbsearch(a,s,mid+1,high);
+	    if (n<0)
+		return rbsearch(a,s,low,mid-1);
+	}
+	if (s.compareTo(a[low])==0)
+	    return s;
+	else
+	    return null;
+    }
+    
+    /*-----------MAIN--------------------*/
     public static void main(String[] args){
+	Search sea = new Search();
+	Comparable[] c = {"hello", "sad", "watermelon","seven"};
+	Arrays.sort(c);
+	System.out.println(Arrays.toString(c));
+	System.out.println(sea.lsearch(c,"water"));
+	System.out.println(sea.lsearch(c,"watermelon"));
+	System.out.println(sea.bsearch(c,"water"));
+	System.out.println(sea.bsearch(c,"watermelon"));
+	System.out.println(sea.rbsearch(c,"water"));
+	System.out.println(sea.rbsearch(c,"watermelon"));
+	
+	
 	
     }
 }
